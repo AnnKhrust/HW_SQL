043c9b0bca77c1660f45115cbf1ad5e527be7bf8
AnnKhrust <anna.khrust@gmail.com>
2023-04-12 21:17:21 +0300

    update select_pro

--- SELECT_pro.sql
+++ SELECT_pro.sql
@@ -14,10 +14,14 @@ JOIN album a ON t.album = a.album_id
 GROUP BY a.name
 
 --Все исполнители, которые не выпустили альбомы в определенном году.
-SELECT s.name  FROM albumssingers als 
-JOIN album a ON als.album_id = a.album_id 
-JOIN singer s ON als.singer_id = s.singer_id 
-WHERE a.release_date != 2021
+SELECT s.name FROM singer s  
+WHERE s.name NOT IN ( 
+    SELECT s2.name 
+    FROM singer s2 
+    JOIN albumssingers als ON als.singer_id  = s2.singer_id 
+    JOIN album a  ON als.album_id  = a.album_id  
+    WHERE a.release_date  = 2021 
+);
 
 --Названия сборников, в которых присутствует конкретный исполнитель
 SELECT c.name FROM collection c 
@@ -28,17 +32,14 @@ JOIN albumssingers a3  ON a.album_id = a3.album_id
 JOIN singer s  ON a3.singer_id = s.singer_id 
 WHERE s.name LIKE 'Queen'
 
---Названия сборников, в которых присутствуют исполнители более чем одного жанра.
-SELECT DISTINCT c.name FROM collection c 
-JOIN collectionstracks c2  ON c.collection_id  = c2.collection_id  
-JOIN track t ON c2.track_id = t.track_id
-JOIN album a ON a.album_id = t.album
-JOIN albumssingers a3  ON a.album_id = a3.album_id
-JOIN singer s  ON a3.singer_id = s.singer_id
-WHERE s.name IN (SELECT s.name FROM genressingers g 
-						JOIN singer s ON g.singer_id  = s.singer_id 
-						GROUP BY s.name
-						HAVING COUNT(*) > 1)
+--Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
+SELECT DISTINCT a.name 
+FROM album a 
+JOIN albumssingers a2  ON a2.album_id = a.album_id 
+JOIN singer s  ON s.singer_id  = a2.singer_id 
+JOIN genressingers g  ON g.singer_id  = a2.singer_id  
+GROUP BY a.name, g.singer_id
+HAVING COUNT(g.genre_id) > 1;
 
 --Наименования треков, которые не входят в сборники.
 SELECT t.name FROM track t 
@@ -53,10 +54,12 @@ JOIN track t ON t.album = a.album_id
 WHERE t.duration = (SELECT MIN(t.duration) FROM track t)
 
 --Названия альбомов, содержащих наименьшее количество треков.
-SELECT c.name, count(ct.track_id)  FROM collection c 
-JOIN collectionstracks ct ON ct.collection_id = c.collection_id 
-JOIN track t ON t.track_id = ct.track_id 
-GROUP BY c.name
-ORDER BY count ASC 
-LIMIT 1
-
+SELECT a.name FROM album a 
+JOIN track t  ON t.album  = a.album_id  
+GROUP BY a.album_id 
+HAVING COUNT(t.album) = (
+    SELECT COUNT(track_id) FROM track
+    GROUP BY album 
+    ORDER BY count /* DESC */ /* проверка выдачи наибольшего количества, т.к. все альбомы, кроме одного, содержат по 1 треку. */
+    LIMIT 1 
+);
